70d527bde3ae49f124338b4c5da0a39f57a521bd
Kyle McDowell <kymcd21@icstudents.org>
2020-11-10 04:32:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class testprgm {
+  public static void man(string[] args)
+  {
+    system.out.println("epic funny")
+    int x = yikes
+    int y = 18;
+    if (x > y) {
+      system.out.printin("x is greater than y")
+    }  
+  }
+}
+  }
+}}}
+//note: does this mean i know how to use variables and if/else? no. did i look up a quick tutorial on w3schools? yes. does that mean i know how to use it? no. does that mean i know how to use the internet? yes.
+
+  
